84b1c5aac828ca2245ecd2ab9d0313234e27ab63
misterevans <joshua.m.evans1@gmail.com>
2014-08-27 14:38:50 -0400

    Test

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace AutoLync
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
+            Application.EnableVisualStyles();
         }
     }
 }
